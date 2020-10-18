992a5ae7aed256a7d2072af32d6f4c8e23f9f450
itirbit <twmw@mail.ru>
2020-10-18 15:04:48 +0000

    sysv shared memory

---
+++ unix/ipc/sh_mem/sysv.c
@@ -0,0 +1,30 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <error.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+	key_t key = ftok("/tmp/mem.tmp", 1);
+	int shmId = shmget(key, (1024*1024), IPC_CREAT | 0666);
+	if (shmId == -1)
+	{
+		perror("failed shmget");
+		return 1;
+	}
+	void* mem = shmat(shmId, NULL, 0);
+	if (*(int*)mem == -1)
+	{
+		perror("failed shmat");
+		return 1;
+	}
+	memset(mem, 42, 1024*1024);
+	
+	//for (int i = 0; i < 1024*1024; ++i)
+	//{
+	//	printf("%d", ((int*) mem)[i]);
+	//}
+	return 0;
+}
+
