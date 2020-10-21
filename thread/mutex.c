4bcc823e631050abfb395b175d6433b459c6c2ed
itirbit <twmw@mail.ru>
2020-10-21 15:02:28 +0000

    write pid

--- thread/mutex.c
+++ thread/mutex.c
@@ -1,4 +1,7 @@
 #include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_spinlock_t spinLock;
@@ -38,12 +41,26 @@ int main(int argc, char** argv)
 	pthread_rwlock_rdlock(&rdLock);
 	pthread_rwlock_wrlock(&wrLock);
 
+	char data[1024];
+	sprintf(data, "%d", getpid());
+
+	FILE* f = fopen("/tmp/main.pid", "w");
+	fputs(data, f);
+	fclose(f);
+
 	pthread_t threads[4];
 
 	pthread_create(&threads[0], NULL, mutex_thread, NULL);
 	pthread_create(&threads[1], NULL, spin_thread, NULL);
 	pthread_create(&threads[2], NULL, rd_thread, NULL);
 	pthread_create(&threads[3], NULL, wr_thread, NULL);
+
+	void* val;
+
+	pthread_join(threads[0], val);
+	pthread_join(threads[1], val);
+	pthread_join(threads[2], val);
+	pthread_join(threads[3], val);
 
 	return 0;
 }
