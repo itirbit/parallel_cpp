db85fa8adf831386ff623a0560428cf3f0c565c5
itirbit <twmw@mail.ru>
2021-01-28 13:59:14 +0000

    boost asio async wait

--- libs/asio/timer/main.cpp
+++ libs/asio/timer/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
+void print(const boost::system::error_code&)
+{
+	std::cout << "Hello, world from print!\n";
+}
+
 int main()
 {
 	boost::asio::io_context io;
 	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
-	t.wait();
-	std::cout << "Hello, world!\n";
+	t.async_wait(&print);
+	io.run();
+	std::cout << "Hello world from main!\n";
 	return 0;
 }
