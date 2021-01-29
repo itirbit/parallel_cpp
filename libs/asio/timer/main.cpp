9c6522e9c0991abd81734f6d946ddf4fc0f351ac
itirbit <twmw@mail.ru>
2021-01-29 12:25:39 +0000

    boost timer 4

--- libs/asio/timer/main.cpp
+++ libs/asio/timer/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
 
-void print(const boost::system::error_code&)
+
+void print(const boost::system::error_code&, boost::asio::steady_timer* t, int* count)
 {
 	std::cout << "Hello, world from print!\n";
+	if (*count < 5)
+	{
+		std::cout << * count << '\n';
+		++(*count);
+		t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+		t->async_wait(boost::bind(print, boost::asio::placeholders::error,t, count));
+	}
 }
 
 int main()
 {
 	boost::asio::io_context io;
-	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
-	t.async_wait(&print);
+	int count;
+	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(1));
+	t.async_wait(boost::bind(print, boost::asio::placeholders::error, &t, &count));
 	io.run();
 	std::cout << "Hello world from main!\n";
+	std::cout << "Final Count is " << count << '\n';
 	return 0;
 }
