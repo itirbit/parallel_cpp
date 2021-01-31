4a8b88c56df425f0d89efe8e36fcbc8ce666189d
itirbit <twmw@mail.ru>
2021-01-31 08:11:06 +0000

    async server

--- libs/asio/daytime/server.cpp
+++ libs/asio/daytime/server.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
 
 using boost::asio::ip::tcp;
 
@@ -12,20 +16,71 @@ std::string make_daytime_string()
 	return ctime(&now);
 }
 
+class tcp_connection : public boost::enable_shared_from_this<tcp_connection>
+{
+public:
+	typedef boost::shared_ptr<tcp_connection> pointer;
+
+	static pointer create(boost::asio::io_context& io_context)
+	{
+		return pointer(new tcp_connection(io_context));
+	}
+
+	tcp::socket& socket()
+	{
+		return sock;
+	}
+
+	void start()
+	{
+		message = make_daytime_string();
+		boost::asio::async_write(sock, boost::asio::buffer(message), boost::bind(&tcp_connection::handle_write, shared_from_this()));
+	}
+private:
+	tcp_connection(boost::asio::io_context& io_context) :
+		sock(io_context){}
+
+	void handle_write(){}
+
+	tcp::socket sock;
+	std::string message;
+};
+
+class tcp_server
+{
+public:
+	tcp_server(boost::asio::io_context& io_context) :
+	io_context(io_context),
+	acceptor(io_context, tcp::endpoint(tcp::v4(),13))
+	{	
+		start_accept();
+	}
+private:
+
+	void start_accept()
+	{
+		tcp_connection::pointer new_connection = tcp_connection::create(io_context);
+		acceptor.async_accept(new_connection->socket(), boost::bind(&tcp_server::handle_accept, this, new_connection, boost::asio::placeholders::error));
+	}
+
+	void handle_accept(tcp_connection::pointer new_connection, const boost::system::error_code& error)
+	{
+		if(!error)
+			new_connection->start();
+		start_accept();
+	}
+
+	boost::asio::io_context& io_context;
+	tcp::acceptor acceptor;
+};	
+
 int main()
 {
 	try
 	{
 		boost::asio::io_context io_context;
-		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(),13));
-		for(;;)
-		{
-			tcp::socket socket(io_context);
-			acceptor.accept(socket);
-			std::string message = make_daytime_string();
-			boost::system::error_code ignored_error;
-			boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
-		}
+		tcp_server server(io_context);
+		io_context.run();
 	}
 	catch(std::exception& e)
 	{
