82e0c7133f7e14ddcd4916aec9abb8c143addab6
itirbit <twmw@mail.ru>
2021-01-31 06:54:34 +0000

    boost timer 5

--- libs/asio/timer/main.cpp
+++ libs/asio/timer/main.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
+#include <boost/thread/thread.hpp>
 
 class printer
 {
 public:
 	printer(boost::asio::io_context& io): 
-		timer(io, boost::asio::chrono::seconds(1)),
+		strand(boost::asio::make_strand(io)),
+		timer1(io, boost::asio::chrono::seconds(1)),
+		timer2(io, boost::asio::chrono::seconds(1)),
 		count(0)
 	{
-		timer.async_wait(boost::bind(&printer::print, this));
+		timer1.async_wait(boost::bind(&printer::print1, this));
+		timer2.async_wait(boost::bind(&printer::print2, this));
 	}
 	~printer()
 	{
 		std::cout << "Final count is " << count << '\n';
 	}
 
-	void print()
+	void print1()
 	{
-		if (count < 5)
+		if (count < 10)
 		{
-			std::cout << count << '\n';
+			std::cout << "Timer 1: " << count << '\n';
 			++count;
-			timer.expires_at(timer.expiry() + boost::asio::chrono::seconds(1));
-			timer.async_wait(boost::bind(&printer::print, this));
+			timer1.expires_at(timer1.expiry() + boost::asio::chrono::seconds(1));
+			timer1.async_wait(boost::asio::bind_executor(strand, boost::bind(&printer::print1, this)));
+		}
+	}
+	void print2()
+	{
+		if (count < 10)
+		{
+			std::cout << "Timer 2: " << count << '\n';
+			++count;
+			timer2.expires_at(timer2.expiry() + boost::asio::chrono::seconds(1));
+			timer2.async_wait(boost::asio::bind_executor(strand, boost::bind(&printer::print2, this)));
 		}
 	}
 private:
-	boost::asio::steady_timer timer;
+	boost::asio::strand<boost::asio::io_context::executor_type> strand;
+	boost::asio::steady_timer timer1;
+	boost::asio::steady_timer timer2;
 	int count;
 };
 
@@ -36,6 +52,8 @@ int main()
 {
 	boost::asio::io_context io;
 	printer p(io);
+	boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
 	io.run();
+	t.join();
 	return 0;
 }
